cb5da19c6c4570c95ae4bf51b5f159cc16d730ac
David J. H. Shih <djh.shih@gmail.com>
2025-01-04 00:49:53 +0800

    Add coin code.

---
+++ class/02/coin.R
@@ -0,0 +1,51 @@
+library(ggplot2)
+
+# outcome is tail or head
+outcomes <- c("T", "H");
+
+# probabilities of the outcomes
+prob <- c(0.3, 0.7);
+
+# ensure that probs sum to 1
+prob <- prob / sum(prob);
+
+# number of coin flips in a sequence
+n.trials <- 10;
+
+# Enumerate the sample space of a sequence of n trials,
+# given the outcomes of individual trials,
+# returning the sample space
+enumerate <- function(outcomes, n) {
+  if (n == 1) {
+    as.list(outcomes)
+  } else {
+    branches <- enumerate(outcomes, n - 1);
+    unlist(
+      lapply(branches,
+        function(b) {
+          lapply(outcomes, function(o) c(b, o))
+        }
+      ),
+      recursive = FALSE
+    )
+  }
+}
+
+sample.space <- enumerate(outcomes, n.trials);
+
+# draw a sample
+s <- sample(S, n.trials, replace=TRUE, prob=prob);
+
+sum(unlist(lapply(sample.space, function(r) all(r == s))))
+
+# we define random variable X as the number of heads
+
+# all values of X in the sample space
+x.values <- unlist(lapply(sample.space, function(r) sum(r == "H")));
+
+# domain
+unique(x.values)
+
+# induced probability function on random variable X
+prob.f <- table(x.values) / length(x.values);
+
