5cb91e1cd147644e75d267e550761c935374be9b
David J. H. Shih <djh.shih@gmail.com>
2025-01-06 12:43:52 +0800

    Refactor code.

--- class/02/coin.R
+++ class/02/coin.R
@@ -4,11 +4,10 @@ library(ggplot2)
 outcomes <- c("T", "H");
 
 # probabilities of each outcome
-#outcomes.prob <- c(0.3, 0.7);
-outcomes.prob <- c(0.5, 0.5);
+probs.outcome <- c(0.3, 0.7);
 
 # ensure that probs sum to 1
-outcomes.prob <- outcomes.prob / sum(outcomes.prob);
+probs.outcome <- probs.outcome / sum(probs.outcome);
 
 # number of coin flips in a sequence
 n.trials <- 10;
@@ -32,55 +31,74 @@ enumerate <- function(outcomes, n) {
   }
 }
 
-sample.space <- enumerate(outcomes, n.trials);
+# sample space
+S <- enumerate(outcomes, n.trials);
 
-# probability of each element in the sample space
-sample.space.prob <- unlist(lapply(
-  enumerate(outcomes.prob, n.trials),
+# probability of each element in sample space S
+probs.S <- unlist(lapply(
+  enumerate(probs.outcome, n.trials),
   function(probs) {
-    # probs contain the probability of each trial
+    # probs contain the probability of trials in a sequence
     # obtain the probability for the sequence by multiplying probs together
     prod(probs)
   }
 ));
 
-sum(sample.space.prob)
-
-# draw a sample s
-s <- sample(outcomes, n.trials, replace=TRUE, prob=outcomes.prob);
-
-sum(unlist(lapply(sample.space, function(r) all(r == s))))
+sum(probs.S)
 
 # we define random variable X as the number of heads
+X <- function(s) {
+  sum(s == "H")
+}
 
 # map each element of the sample space onto the domain of X
-sample.space.x <- unlist(lapply(sample.space, function(r) sum(r == "H")));
+values.x <- unlist(lapply(S, X));
 
 # domain of random variable X
-domain.x <- unique(sample.space.x);
+domain.x <- unique(values.x);
 
 # induced probability function on random variable X
-prob.x <- unlist(lapply(domain.x,
+probs.x <- unlist(lapply(domain.x,
   function(x) {
-    idx <- sample.space.x == x;
-    sum(sample.space.prob[idx])
+    # index of elements in the sample space that correspond to realization x
+    # this index is used to extract the samples that correspond to 
+    # the event where X = x
+    idx <- values.x == x;
+    sum(probs.S[idx])
   }
 ));
 
-print(prob.x)
+print(probs.x)
 
-ggplot(data.frame(x=domain.x, y=prob.x), aes(x, y)) + theme_classic() +
+ggplot(data.frame(x=domain.x, y=probs.x), aes(x, y)) + theme_classic() +
   geom_col()
+
+
+# draw a sample s
+s <- sample(outcomes, n.trials, replace=TRUE, prob=probs.outcome);
+
+# find the index of sample s in the sample space
+s.idx <- which(unlist(lapply(S, function(s.i) all(s.i == s))));
+probs.S[s.idx]
+
+# map sample s of sample space to realization x
+X(s)
+
 
 
 # Remarks
 # 1. Uniform probability in the sample space induced a non-uniform probability
-#    distribution on X (number of heads)
+#    distribution on X (number of heads).
+# 2. Mathematical functions can be represented as vectors.
+
 
 # Questions
-# 1. Why do we sample with replacement?
+# 1. When we are drawing coin flips, why do we sample with replacement?
 # 2. How does changing the probability function on the sample space change
 #    the induced probability function on X?
+# 3. How can we sample the random variable X directly? This can be implemented
+#    in one-line of code.
+
 
 # Bonus questions
 # 1. As the number of trials increases, can we still calculate `prob.x`
